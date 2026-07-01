f647ef560f0830002ce62df37269e7723277651d
toolshub26 <tools.hub26@gmail.com>
2026-07-01 19:36:33 +0530

    fix(functions): prevent duplicate Firebase initialization

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,27 @@
+"use strict";
 
+const crypto = require("crypto");
+const Razorpay = require("razorpay");
+
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+
+if (!admin.apps.length) {
+    admin.initializeApp();
+}
+
+const db = admin.firestore();
+
+const razorpay = new Razorpay({
+    key_id: functions.config().razorpay.key_id,
+    key_secret: functions.config().razorpay.key_secret
+});
+
+exports.health = functions.https.onRequest((req, res) => {
+    res.status(200).json({
+        success: true,
+        service: "Lexora Cloud Functions",
+        version: "1.0.0",
+        timestamp: Date.now()
+    });
+});
