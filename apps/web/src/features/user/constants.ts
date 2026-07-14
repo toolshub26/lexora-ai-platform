d4dfdb276108211e07810f688588968304ab3868
toolshub26 <tools.hub26@gmail.com>
2026-07-14 18:59:48 +0530

    feat(user): add user constants

---
+++ apps/web/src/features/user/constants.ts
@@ -0,0 +1,16 @@
+export const USER_ROLES = [
+  "user",
+  "admin",
+] as const;
+
+export const USER_PLANS = [
+  "free",
+  "pro",
+  "enterprise",
+] as const;
+
+export const DEFAULT_LANGUAGE = "en";
+
+export const DEFAULT_THEME = "system";
+
+export const DEFAULT_NOTIFICATIONS = true;
