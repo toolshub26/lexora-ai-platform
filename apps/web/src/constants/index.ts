0afd72185f6fef3793606609b24b5b0faf259f54
toolshub26 <tools.hub26@gmail.com>
2026-07-14 14:34:15 +0530

    feat(constants): add application constants

---
+++ apps/web/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = "Lexora AI";
+
+export const APP_VERSION = "1.0.0";
+
+export const COMPANY_NAME = "Lexora AI Platform";
+
+export const COPYRIGHT_YEAR = new Date().getFullYear();
