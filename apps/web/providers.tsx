8f94ffc1c07d58e881a7827611a771a108bdcdb6
toolshub26 <tools.hub26@gmail.com>
2026-07-18 19:48:32 +0530

    feat(web): add root providers

---
+++ apps/web/providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ReactNode } from "react";
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+export default function Providers({
+  children,
+}: ProvidersProps) {
+  return <>{children}</>;
+}
