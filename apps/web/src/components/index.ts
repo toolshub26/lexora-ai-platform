d9cae26144157d36578fa653ca4510b0c7b223af
toolshub26 <tools.hub26@gmail.com>
2026-07-19 12:02:42 +0530

    feat(ui): export enterprise UI components

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,19 @@
+export { default as Alert } from "./alert";
+export { default as Badge } from "./badge";
+export { default as Button } from "./button";
+export { default as Card } from "./card";
+export { default as Input } from "./input";
+export { default as Modal } from "./modal";
+export { default as Select } from "./select";
+export { default as Spinner } from "./spinner";
+export { default as Textarea } from "./textarea";
+
+export type { AlertProps } from "./alert";
+export type { BadgeProps } from "./badge";
+export type { ButtonProps } from "./button";
+export type { CardProps } from "./card";
+export type { InputProps } from "./input";
+export type { ModalProps } from "./modal";
+export type { SelectProps } from "./select";
+export type { SpinnerProps } from "./spinner";
+export type { TextareaProps } from "./textarea";
