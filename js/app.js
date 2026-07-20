5320d02056600855ecc4c3d7a23fbb88319d73e5
toolshub26 <tools.hub26@gmail.com>
2026-07-20 18:02:13 +0530

    fix(app): close registerUIEvents function

--- js/app.js
+++ js/app.js
@@ -442,6 +442,7 @@ document.querySelectorAll(".dashboard-card").forEach(card => {
 });
 
 });
+};  
 /* ==========================================
 Extend registerEvents
 ========================================== */
